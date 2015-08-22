bd88440fc62f481f1d0b0893b650faf006cba659
Yngar <yngarthewise@gmail.com>
2015-08-22 14:44:00 -0500

    up to page 6

--- assets/entities.js
+++ assets/entities.js
@@ -4,17 +4,30 @@ Game.Mixins = {};
 // Define our Moveable mixin
 Game.Mixins.Moveable = {
     name: 'Moveable',
-    tryMove: function(x, y, map) {
+    tryMove: function (x, y, map) {
         var tile = map.getTile(x, y);
-        // Check if we can walk on the tile
-        // and if so simply walk onto it
-        if (tile.isWalkable()) {
+        var target = map.getEntityAt(x, y);
+        // If an entity was present at the tile
+        if (target) {
+            // If we are an attacker, try to attack
+            // the target
+            if (this.hasMixin('Attacker')) {
+                this.attack(target);
+                return true;
+            } else {
+                // If not nothing we can do, but we can't 
+                // move to the tile
+                return false;
+            }
+            // Check if we can walk on the tile
+            // and if so simply walk onto it
+        } else if (tile.isWalkable()) {
             // Update the entity's position
             this._x = x;
             this._y = y;
             return true;
-        // Check if the tile is diggable, and
-        // if so try to dig it
+            // Check if the tile is diggable, and
+            // if so try to dig it
         } else if (tile.isDiggable()) {
             map.dig(x, y);
             return true;
@@ -23,10 +36,88 @@ Game.Mixins.Moveable = {
     }
 }
 
+// Main player's actor mixin
+Game.Mixins.PlayerActor = {
+    name: 'PlayerActor',
+    groupName: 'Actor',
+    act: function () {
+        // Re-render the screen
+        Game.refresh();
+        // Lock the engine and wait asynchronously
+        // for the player to press a key.
+        this.getMap().getEngine().lock();
+    }
+}
+Game.Mixins.FungusActor = {
+    name: 'FungusActor',
+    groupName: 'Actor',
+    init: function() {
+        this._growthsRemaining = 5;
+    },
+    act: function () {
+        // Check if fungus grows this turn
+        if (this._growthsRemaining > 0) {
+            if (Math.random() <= 0.02) {
+                //Generate the coordinates of a random adjacent square by
+                //generating an offset between [-1, 0, 1] for both the x 
+                //and y directions. random rolls 0-2 -1
+                var xOffset = Math.floor(Math.random() * 3) - 1;
+                var yOffset = Math.floor(Math.random() * 3) - 1;
+                //make sure its not this tile
+                if (xOffset != 0 || yOffset != 0) {
+                    //check if location is spawnable then grow
+                    if(this.getMap().isEmptyFloor(this.getX() + xOffset,
+                                                this.getY() + yOffset)) {
+                        var entity = new Game.Entity(Game.FungusTemplate);
+                        entity.setX(this.getX() + xOffset);
+                        entity.setY(this.getY() + yOffset);
+                        this.getMap().addEntity(entity);
+                        this._growthsRemaining--;
+                    }
+                }
+            }
+        }
+    }
+}
+
+// This signifies our entity can attack basic destructible enities
+Game.Mixins.SimpleAttacker = {
+    name: 'SimpleAttacker',
+    groupName: 'Attacker',
+    attack: function (target) {
+        // Only remove the entity if they were attackable
+        if (target.hasMixin('Destructible')) {
+            target.takeDamage(this, 1);
+        }
+    }
+}
+
+// This mixin signifies an entity can take damage and be destroyed
+Game.Mixins.Destructible = {
+    name: 'Destructible',
+    init: function () {
+        this._hp = 1;
+    },
+    takeDamage: function (attacker, damage) {
+        this._hp -= damage;
+        // If have 0 or less HP, then remove ourseles from the map
+        if (this._hp <= 0) {
+            this.getMap().removeEntity(this);
+        }
+    }
+}
+
 // Player template
 Game.PlayerTemplate = {
     character: '@',
     foreground: 'white',
     background: 'black',
-    mixins: [Game.Mixins.Moveable]
+    mixins: [Game.Mixins.Moveable, Game.Mixins.PlayerActor,
+                Game.Mixins.SimpleAttacker, Game.Mixins.Destructible]
+}
+// Fungus template
+Game.FungusTemplate = {
+    character: 'F',
+    foreground: 'green',
+    mixins: [Game.Mixins.FungusActor, Game.Mixins.Destructible]
 }
