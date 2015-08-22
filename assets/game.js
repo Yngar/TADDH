c451b1c9193304b0e7088eb511579ebd939fd0f8
Yngar <yngarthewise@gmail.com>
2015-08-22 10:38:25 -0500

    end of section 4

--- assets/game.js
+++ assets/game.js
@@ -1,29 +1,42 @@
 var Game =  {
 	_display: null,
-	_currentScreen: null,
+    _currentScreen: null,
+    _screenWidth: 80,
+    _screenHeight: 24,
 	init: function() {
-	    // Any necessary initialization will go here.
-	    this._display = new ROT.Display({width: 80, height: 24});
-	    // Create a helper function for binding to an event
-	    // and making it send it to the screen
-	    var game = this; // So that we don't lose this
-	    var bindEventToScreen = function(event) {
-	        window.addEventListener(event, function(e) {
-	            // When an event is received, send it to the
-	            // screen if there is one
-	            if (game._currentScreen !== null) {
-	                // Send the event type and data to the screen
-	                game._currentScreen.handleInput(event, e);
-	            }
-	        });
-	    }
-	    // Bind keyboard input events
-	    bindEventToScreen('keydown');
-	    bindEventToScreen('keyup');
-	    bindEventToScreen('keypress');
-	},
+        // Any necessary initialization will go here.
+        this._display = new ROT.Display({width: this._screenWidth,
+                                         height: this._screenHeight});
+        // Create a helper function for binding to an event
+        // and making it send it to the screen
+        var game = this; // So that we don't lose this
+        var bindEventToScreen = function(event) {
+            window.addEventListener(event, function(e) {
+                // When an event is received, send it to the
+                // screen if there is one
+                if (game._currentScreen !== null) {
+                    // Send the event type and data to the screen
+                    game._currentScreen.handleInput(event, e);
+                    // Clear the screen
+                    game._display.clear();
+                    // Render the screen
+                    game._currentScreen.render(game._display);
+                }
+            });
+        }
+        // Bind keyboard input events
+        bindEventToScreen('keydown');
+        //bindEventToScreen('keyup');
+        //bindEventToScreen('keypress');
+    },
 	getDisplay: function() {
 		return this._display;
+	},
+	getScreenWidth: function() {
+    return this._screenWidth;
+	},
+	getScreenHeight: function() {
+	    return this._screenHeight;
 	},
 	switchScreen: function(screen) {
 	    // If we had a screen before, notify it that we exited
@@ -55,4 +68,3 @@ window.onload = function() {
         Game.switchScreen(Game.Screen.startScreen);
     }
 }
-
--- assets/screens.js
+++ assets/screens.js
@@ -21,18 +21,23 @@ Game.Screen.startScreen = {
 
 // Define our playing screen
 Game.Screen.playScreen = {
+    _map: null,
+    _player: null,
     enter: function() {  
         var map = [];
-        for (var x = 0; x < 80; x++) {
+        // Create a map based on our size parameters
+        var mapWidth = 500;
+        var mapHeight = 500;
+        for (var x = 0; x < mapWidth; x++) {
             // Create the nested array for the y values
             map.push([]);
             // Add all the tiles
-            for (var y = 0; y < 24; y++) {
+            for (var y = 0; y < mapHeight; y++) {
                 map[x].push(Game.Tile.nullTile);
             }
         }
         // Setup the map generator
-        var generator = new ROT.Map.Cellular(80, 24);
+        var generator = new ROT.Map.Cellular(mapWidth, mapHeight);
         generator.randomize(0.5);
         var totalIterations = 3;
         // Iteratively smoothen the map
@@ -49,28 +54,107 @@ Game.Screen.playScreen = {
         });
         // Create our map from the tiles
         this._map = new Game.Map(map);
+        // Create our player and set the position
+        this._player = new Game.Entity(Game.PlayerTemplate);
+        var position = this._map.getRandomFloorPosition();
+        this._player.setX(position.x);
+        this._player.setY(position.y);
     },
     exit: function() { console.log("Exited play screen."); },
     render: function(display) {
-        // Iterate through all map cells
-        for (var x = 0; x < this._map.getWidth(); x++) {
-            for (var y = 0; y < this._map.getHeight(); y++) {
+        var screenWidth = Game.getScreenWidth();
+        var screenHeight = Game.getScreenHeight();
+        // Make sure the x-axis doesn't go to the left of the left bound
+        var topLeftX = Math.max(0, this._player.getX() - (screenWidth / 2));
+        // Make sure we still have enough space to fit an entire game screen
+        topLeftX = Math.min(topLeftX, this._map.getWidth() - screenWidth);
+        // Make sure the y-axis doesn't above the top bound
+        var topLeftY = Math.max(0, this._player.getY() - (screenHeight / 2));
+        // Make sure we still have enough space to fit an entire game screen
+        topLeftY = Math.min(topLeftY, this._map.getHeight() - screenHeight);
+        // Iterate through all visible map cells
+        for (var x = topLeftX; x < topLeftX + screenWidth; x++) {
+            for (var y = topLeftY; y < topLeftY + screenHeight; y++) {
                 // Fetch the glyph for the tile and render it to the screen
-                var glyph = this._map.getTile(x, y).getGlyph();
-                display.draw(x, y,
-                    glyph.getChar(), 
-                    glyph.getForeground(), 
-                    glyph.getBackground());
+                // at the offset position.
+                var tile = this._map.getTile(x, y);
+                display.draw(
+                    x - topLeftX,
+                    y - topLeftY,
+                    tile.getChar(), 
+                    tile.getForeground(), 
+                    tile.getBackground())
             }
         }
+        // Render the player
+        display.draw(
+            this._player.getX() - topLeftX, 
+            this._player.getY() - topLeftY,    
+            this._player.getChar(), 
+            this._player.getForeground(), 
+            this._player.getBackground()
+        );
     },
     handleInput: function(inputType, inputData) {
         if (inputType === 'keydown') {
-
+            // If enter is pressed, go to the win screen
+            // If escape is pressed, go to lose screen
             if (inputData.keyCode === ROT.VK_RETURN) {
+                Game.switchScreen(Game.Screen.winScreen);
             } else if (inputData.keyCode === ROT.VK_ESCAPE) {
+                Game.switchScreen(Game.Screen.loseScreen);
+            }
+            // Movement
+            if (inputData.keyCode === ROT.VK_LEFT) {
+                this.move(-1, 0);
+            } else if (inputData.keyCode === ROT.VK_RIGHT) {
+                this.move(1, 0);
+            } else if (inputData.keyCode === ROT.VK_UP) {
+                this.move(0, -1);
+            } else if (inputData.keyCode === ROT.VK_DOWN) {
+                this.move(0, 1);
             }
         }    
+    },
+    move: function(dX, dY) {
+        var newX = this._player.getX() + dX;
+        var newY = this._player.getY() + dY;
+        // Try to move to the new cell
+        this._player.tryMove(newX, newY, this._map);
     }
 }
 
+// Define our winning screen
+Game.Screen.winScreen = {
+    enter: function() { console.log("Entered win screen."); },
+    exit: function() { console.log("Exited win screen."); },
+    render: function(display) {
+        // Render our prompt to the screen
+        for (var i = 0; i < 22; i++) {
+            // Generate random background colors
+            var r = Math.round(Math.random() * 255);
+            var g = Math.round(Math.random() * 255);
+            var b = Math.round(Math.random() * 255);
+            var background = ROT.Color.toRGB([r, g, b]);
+            display.drawText(2, i + 1, "%b{" + background + "}You win!");
+        }
+    },
+    handleInput: function(inputType, inputData) {
+        // Nothing to do here      
+    }
+}
+
+// Define our winning screen
+Game.Screen.loseScreen = {
+    enter: function() {    console.log("Entered lose screen."); },
+    exit: function() { console.log("Exited lose screen."); },
+    render: function(display) {
+        // Render our prompt to the screen
+        for (var i = 0; i < 22; i++) {
+            display.drawText(2, i + 1, "%b{red}You lose! :(");
+        }
+    },
+    handleInput: function(inputType, inputData) {
+        // Nothing to do here      
+    }
+}
